2c2d3099ac1fefd211c232f0fbf35d7380628702
Tu TV <tutv95@gmail.com>
2022-11-20 01:28:09 +0700

    udpated

--- src/app/v3/DownloadCV.js
+++ src/app/v3/DownloadCV.js
@@ -1,9 +1,15 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faCloudDownload} from "@fortawesome/free-solid-svg-icons"
+
 const fileName = 'Tran Van Tu - Senior Software Engineer.pdf'
 
 export default function DownloadCV() {
     return (
         <div className="DownloadCV">
-            <a href="/pdf/cv.pdf" download={fileName}>Download</a>
+            <a href="/pdf/cv.pdf" download={fileName}>
+                <FontAwesomeIcon icon={faCloudDownload}/>
+                <span> Download</span>
+            </a>
         </div>
     )
 }
